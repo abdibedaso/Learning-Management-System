41c7f627f0071e0eaf8cbe6c54510950ecc3400e
Abdi T Bedaso <atbedaso@gmail.com>
2021-11-14 20:02:54 -0500

    init backend proj

---
+++ lms-backend/app.js
@@ -0,0 +1,12 @@
+// const winston = require("winston");
+const express = require("express");
+const config = require("config");
+const app = express();
+
+
+const port = process.env.PORT || config.get("port");
+const server = app.listen(port, () =>
+    console.log(`Listening on port ${port}...`)
+);
+
+module.exports = server;
