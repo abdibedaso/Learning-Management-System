2219fcb8d7b995259d35cd6a51346351de6f2a87
Abdi T Bedaso <atbedaso@gmail.com>
2021-11-14 20:51:52 -0500

    require midlwares n routes in app

--- lms-backend/app.js
+++ lms-backend/app.js
@@ -3,6 +3,11 @@ const express = require("express");
 const config = require("config");
 const app = express();
 
+require("./startup/logging")();
+require("./startup/cors")(app);
+require("./startup/routes")(app);
+require("./startup/db")();
+require("./startup/config")(app);
 
 const port = process.env.PORT || config.get("port");
 const server = app.listen(port, () =>
---
+++ lms-backend/startup/routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const users = require('../routes/users');
+const auth = require('../routes/auth');
+const error = require('../middleware/error');
+
+module.exports = function(app) {
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
+    app.use('/api/users', users);
+    app.use('/api/auth', auth);
+    app.use(error);
+}
